3360383704596836a480c5ae7584198f06ea0d84
Leonardo Souza <leogsouza@gmail.com>
2019-03-03 12:57:51 -0300

    Working on the App Adding a TextInput

--- rncourse/App.js
+++ rncourse/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,14 +17,28 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangeHandler = (val) => {
+    this.setState({
+      placeName: val
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          style={{
+            width: 300,
+            borderColor: "black",
+            borderWidth: 1 
+          }}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangeHandler} />
       </View>
     );
   }
